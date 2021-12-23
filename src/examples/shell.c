64d4619808634736dd325b56c21fffa715b837c7
ewzeng <edwardzeng@berkeley.edu>
2021-12-23 08:13:44 +0000

    Pintos Fun: Advanced shell and new utilities for CS 162

--- src/examples/shell.c
+++ src/examples/shell.c
@@ -3,17 +3,26 @@
 #include <string.h>
 #include <syscall.h>
 
+#define COMMAND_BUFLEN 80
+#define DEFAULT_PATH "/bin/"
+
+static pid_t resolve_exec(char* filepath);
 static void read_line(char line[], size_t);
 static bool backspace(char** pos, char line[]);
 
+size_t default_pathlen;
+
 int main(void) {
   printf("Shell starting...\n");
+  default_pathlen = strlen(DEFAULT_PATH);
   for (;;) {
-    char command[80];
+    char commandbuf[COMMAND_BUFLEN + default_pathlen];
+    char* command = commandbuf + default_pathlen;
+    strlcpy(commandbuf, DEFAULT_PATH, default_pathlen + 1);
 
     /* Read command. */
     printf("--");
-    read_line(command, sizeof command);
+    read_line(command, COMMAND_BUFLEN);
 
     /* Execute command. */
     if (!strcmp(command, "exit"))
@@ -24,7 +33,7 @@ int main(void) {
     } else if (command[0] == '\0') {
       /* Empty command. */
     } else {
-      pid_t pid = exec(command);
+      pid_t pid = resolve_exec(command);
       if (pid != PID_ERROR)
         printf("\"%s\": exit code %d\n", command, wait(pid));
       else
@@ -34,6 +43,29 @@ int main(void) {
 
   printf("Shell exiting.");
   return EXIT_SUCCESS;
+}
+
+pid_t resolve_exec(char* filepath) {
+  int fd;
+  char *slashptr, *spaceptr;
+  char* name = filepath;
+  while (*name == ' ') {
+    *name = '/';
+    name++;
+  }
+  spaceptr = strchr(name, ' ');
+  if (spaceptr)
+    *spaceptr = '\0';
+  fd = open(name);
+  if (spaceptr)
+    *spaceptr = ' ';
+  if (fd == -1) {
+    slashptr = strchr(name, '/');
+    if (!slashptr || (spaceptr && slashptr >= spaceptr))
+      return exec(filepath - default_pathlen);
+  } else
+    close(fd);
+  return exec(name);
 }
 
 /* Reads a line of input from the user into LINE, which has room
